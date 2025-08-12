f3327c43a6ed8b4beefee548c3a1a187bbfd0bea
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2025-08-11 21:47:04 -0300

    feat: Atv useState PT2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import EffectCounter from "@/components/EffectCounter";
+import LightState from "@/components/LightState";
 import PlaceForm from "@/components/PlaceForm";
 
 export default function Home() {
   return (
     <div className="flex justify-center">
       {/* <PlaceForm/> */}
-      <EffectCounter/>
+      {/* <EffectCounter/> */}
+      <LightState/>
     </div>
   );
 }
